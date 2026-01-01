f3a9074703efe669d15bb0ed73e75d2cc0d178ba
GauntRecluse <introvertpale@gmail.com>
2026-01-01 23:39:56 +0100

    implement config via Kaleido

--- src/main/java/io/github/mildesttoucan/nonlethal_falls/NLFConfig.java
+++ src/main/java/io/github/mildesttoucan/nonlethal_falls/NLFConfig.java
@@ -1,7 +1,25 @@
 package io.github.mildesttoucan.nonlethal_falls;
 
-public class NLFConfig {
+import folk.sisby.kaleido.api.WrappedConfig;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
 
+public class NLFConfig extends WrappedConfig {
+
+    @Comment("Whether to keep the mod on!")
+    public boolean enabled = true;
+
+    @Comment("Whether the mod should apply to *all* entities (if true overrides the next two options)")
+    public boolean allEntitiesImmunity = false;
+
+    @Comment("Whether the mod should apply to players.")
+    public boolean playerImmunity = true;
+
+    @Comment("Whether the mod should apply to tameable entities (e.g. wolves/dogs, cats, etc.)")
+    public boolean tameableImmunity = true;
+
+    @Comment("How much health to let the entity live on after taking lethal fall damage")
+    @Comment("It's strongly recommended to leave this very low, otherwise players might be able to *heal* by jumping to their not-death!")
+    public float healthLeft = 1.0F;
 
 
 }
--- src/main/java/io/github/mildesttoucan/nonlethal_falls/NonLethalFalls.java
+++ src/main/java/io/github/mildesttoucan/nonlethal_falls/NonLethalFalls.java
@@ -6,22 +6,41 @@ import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.TamableAnimal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Paths;
 
 public class NonLethalFalls implements ModInitializer {
 	public static final String MOD_ID = "nonlethal-falls";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final NLFConfig CONFIG = NLFConfig.createToml(
+            Paths.get("config"),
+            "",
+            MOD_ID,
+            NLFConfig.class
+    );
+
+
 	@Override
     public void onInitialize() {
         ServerLivingEntityEvents.ALLOW_DEATH.register((entity, damageSource, damageAmount) -> {
-            if (!damageSource.is(DamageTypes.FALL) || !(entity instanceof ServerPlayer player)) {
+            if (!damageSource.is(DamageTypes.FALL) || !CONFIG.enabled) {
                 return true;
             }
-            player.setHealth(1.0F);
-            return false;
+
+            if (CONFIG.allEntitiesImmunity) {
+                entity.setHealth(CONFIG.healthLeft);
+                return false;
+            }
+            if ((CONFIG.playerImmunity && entity instanceof ServerPlayer) || (CONFIG.tameableImmunity && entity instanceof TamableAnimal)) {
+                entity.setHealth(CONFIG.healthLeft);
+                return false;
+            }
+            return true;
         });
     }
 }
